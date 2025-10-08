d299bd9afed01e5b1d99278960a7ddba09de52d8
ductran <manhduc2711@gmail.com>
2025-10-08 17:25:43 +0700

    Initial commit

---
+++ src/main/java/com/jw/evoucher/EVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.jw.evoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVoucherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvoucherApplication.class, args);
+    }
+
+}
