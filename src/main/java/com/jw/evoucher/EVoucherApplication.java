14bcfafcaf705d840736cb2060014f796dcc2102
ductran <manhduc2711@gmail.com>
2025-10-08 17:29:16 +0700

    clean init

--- src/main/java/com/jw/evoucher/EVoucherApplication.java
+++ src/main/java/com/jw/evoucher/EVoucherApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EVoucherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EvoucherApplication.class, args);
+        SpringApplication.run(EVoucherApplication.class, args);
     }
 
 }
